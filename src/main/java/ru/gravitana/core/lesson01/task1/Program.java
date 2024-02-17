8510a1443f56153396784fdb736d583ca8854d1e
Виталий Грахов <v.a.grahov@yandex.ru>
2024-02-17 22:09:01 +0500

    console

--- src/main/java/ru/gravitana/core/lesson01/task1/Program.java
+++ src/main/java/ru/gravitana/core/lesson01/task1/Program.java
@@ -3,6 +3,12 @@ package ru.gravitana.core.lesson01.task1;
 import ru.gravitana.core.lesson01.services.Calculator;
 import ru.gravitana.core.lesson01.utils.Decorator;
 
+/*
+ * javac -sourcepath ./java -d out java/ru/gravitana/core/lesson01/task1/Program.java
+ * java -classpath ./out ru.gravitana.core.lesson01.task1.Program
+ * javadoc -encoding utf8 -d docs -sourcepath ./java -cp ./out -subpackages ru
+ */
+
 /**
  * Основной класс приложения. Здесь мы можем описать
  * его основное назначение и способы взаимодействия с ним.
